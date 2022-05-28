867591bbda30992a0975d277f7759b8907ce099b
AbdulazizYas <abdulazizyass@gmail.com>
2022-05-28 17:22:14 +0300

    initial structure

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function buildStyles() {
+  return src("scss/**/*.scss").pipe(sass()).pipe(dest("css"));
+}
+
+function watchTasks() {
+  watch(["scss/**/*.scss"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTasks);

b3b9856f3cfdbbb27daae178d52a8bbda937aacb
AbdulazizYas <abdulazizyass@gmail.com>
2022-06-01 16:11:13 +0300

    Basic structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("scss/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("sass/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTasks() {
-  watch(["scss/**/*.scss"], buildStyles);
+  watch(["sass/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTasks);

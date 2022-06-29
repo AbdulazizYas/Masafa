663eccf71b4a91e7337c4bc87fdb6eb968fa2caa
AbdulazizYas <abdulazizyass@gmail.com>
2022-06-29 21:17:14 +0300

    minify css file v0.8

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const purgecss = require("gulp-purgecss");
+const rename = require("gulp-rename");
+const clean = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+//const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
   return (
     src("masafa/**/*.scss")
       .pipe(sass())
       //.pipe(purgecss({ content: ["*.html"] }))
+      .pipe(rename("masafa.css"))
       .pipe(dest("css"))
   );
+}
+
+function minifyCSS() {
+  return src("css/masafa.css")
+    .pipe(sourcemaps.init())
+    .pipe(rename("masafa.min.css"))
+    .pipe(clean())
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("css"));
 }
 
 function watchTasks() {
   watch(["masafa/**/*.scss", "*.html"], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTasks);
+exports.default = series(buildStyles, minifyCSS, watchTasks);
